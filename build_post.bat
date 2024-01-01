5824e53fe9783bd3fa2a0063d91d1ba4b24ee25a
1A3 <mcplayer1a3@gmail.com>
2024-01-01 16:39:21 +0000

    build cleanup

--- build_post.bat
+++ build_post.bat
@@ -1,3 +1,3 @@
 cd C:\Users\Jenson\source\repos\LC-LobbyInviteOnly
-copy ".\LobbyInviteOnly\bin\Debug\.netframework,version=v4.8\LobbyInviteOnly.dll" ".\Output\LobbyInviteOnly.dll" /Y
-copy ".\LobbyInviteOnly\bin\Debug\.netframework,version=v4.8\LobbyInviteOnly.dll" "C:\Users\Jenson\AppData\Roaming\r2modmanPlus-local\LethalCompany\profiles\Default\BepInEx\plugins\Dev1A3-LobbyInviteOnly\LobbyInviteOnly.dll" /Y
+copy ".\LobbyInviteOnly\bin\Release\netstandard2.1\LobbyInviteOnly.dll" ".\Output\LobbyInviteOnly.dll" /Y
+copy ".\LobbyInviteOnly\bin\Release\netstandard2.1\LobbyInviteOnly.dll" "C:\Users\Jenson\AppData\Roaming\r2modmanPlus-local\LethalCompany\profiles\Default\BepInEx\plugins\Dev1A3-LobbyInviteOnly\LobbyInviteOnly.dll" /Y
